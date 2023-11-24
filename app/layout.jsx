3e93fa72379ad2f07afec889941dd0d4475ba8b9
Snowie Dev <lautarochini4@gmail.com>
2023-11-24 15:45:09 -0300

    projects Fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,12 +6,20 @@ import { GeistSans } from "geist/font";
 
 export const metadata = {
   title: "Lautaro Chini",
-  description: "My Portfolio",
+  description: "Welcome to my portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:url" content="https://www.chini-dev.me" />
+        <meta
+          property="og:image"
+          content="https://cdn.discordapp.com/attachments/1012060083720556645/1177680812821450783/Lautaro_Chini.png?ex=657363ba&is=6560eeba&hm=a3590524c107df5bc8e6dbc674b0ab93c589a540f43f5da8a62502845d392717&"
+        />
+      </head>
+
       <body
         className={`${GeistSans.className} antialiased w-full flex flex-col justify-center items-center`}
       >
